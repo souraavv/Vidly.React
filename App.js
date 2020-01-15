78d14a90fbe7e2ed043c5abd0350ba141887e545
Sourav Sharma <46745355+vsourav@users.noreply.github.com>
2020-01-15 17:58:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Movies from "./components/movies";
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Movies></Movies>
+      </main>
+    );
+  }
+}
+
+export default App;
